eb7937b2d9116c474f61d11c3f9b219bcfef2eb1
HoreichFawad <168718621+HoreichFawad@users.noreply.github.com>
2024-05-29 10:03:32 +0200

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+
+#include "mbed.h"
+#include "w5500_header.hpp"
+
+
+int main() {
+    WIZnet_Chip obj;
+    obj.enable();
+    // uint8_t buf[6] 
+    uint8_t buffer=0b11011000; 
+            //       10011000
+    obj.spiWrite(0x001C,0x04,&buffer,1);
+    obj.spiRead(0x001C,0x00,&buffer,1);
+    uint8_t version=obj.getVERSIONR();
+    printf("Version is %d\n",version);
+    printf("%d\n",buffer);
+}          
