3f60f42d6970bc920f513ce5b9d21be2b7ad0bbd
HoreichFawad <168718621+HoreichFawad@users.noreply.github.com>
2024-06-13 14:31:23 +0200

    TCP client

--- example/w5500example.cpp
+++ example/w5500example.cpp
@@ -8,6 +8,7 @@
 const char * IP_Addr    = "192.168.13.164";
 const char * IP_Subnet  = "255.255.255.0";
 const char * IP_Gateway = "192.168.11.1";
+const char * DIP_Addr    = "192.168.13.165";
 uint8_t MAC_Addr[6] = {0x00,0x08,0xDC,0x12,0x34,0x56};
 SPI spi(PB_15, PB_14, PB_13); // mosi, miso, sclk
 EthernetInterface net(&spi, PB_12, PA_10);
@@ -31,8 +32,8 @@ uint32_t strToIP_(const char *str)
     return ip;
 }
 
-int serverExampleTCP() {
-    
+int serverExampleTCP() 
+{    
     spi.format(8,0); // 8bit, mode 0
     spi.frequency(1000000); // 1MHz
     wait_us(1000*1000); // 1 second for stable state
@@ -48,8 +49,8 @@ int serverExampleTCP() {
     net.reg_wr<uint32_t>(SIPR, ip);
     buffer=0x01;
     net.spiWrite(IMR,0x04,&buffer,1);
-    net.sreg<uint16_t>(0, Sn_PORT, 80);
     net.sreg<uint8_t>(0, Sn_MR, Sn_MR_TCP);
+    net.sreg<uint16_t>(0, Sn_PORT, 80);
     net.scmd(0, W5500::Command::OPEN);
     uint16_t buf=0;
     buf=net.getSn_PORT(0);
@@ -84,12 +85,13 @@ int serverExampleTCP() {
     }
     buffer=0;
     net.spiWrite(SIR,0x04,&buffer,1);
-    wait_us(3000*1000);
+    do{
+        buffer=net.sreg<uint8_t>(0, Sn_IR);
+    }while(buffer!=5);
+    // wait_us(5000*1000);
     t.reset();
     t.start();
     int size, size2;
-    while (1)
-    {
         // int size = sreg<uint16_t>(socket, Sn_RX_RSR);
         
         // during the reading Sn_RX_RSR, it has the possible change of this register.
@@ -99,15 +101,12 @@ int serverExampleTCP() {
             size = net.sreg<uint16_t>(0, Sn_RX_RSR);
             size2 = net.sreg<uint16_t>(0, Sn_RX_RSR);
         } while (size != size2);
-            printf("size is %d\n",size);
-            break;
             // return size;
 
         // if (wait_time_ms != (-1) && t.read_ms() > wait_time_ms)
         // {
         //     break;
         // }
-    }
     uint16_t ptr = net.sreg<uint16_t>(0, Sn_RX_RD);
     uint8_t cntl_byte = (0x18 + (0 << 5));
     char buffer2[256];
@@ -139,20 +138,20 @@ int serverExampleTCP() {
     net.sreg<uint16_t>(0, Sn_TX_WR, ptr + len);
     net.scmd(0, W5500::SEND);
     uint8_t tmp_Sn_IR;
-    while (((tmp_Sn_IR = net.sreg<uint8_t>(0, Sn_IR)) & net.INT_SEND_OK) != W5500::INT_SEND_OK)
+    while (((tmp_Sn_IR = net.sreg<uint8_t>(0, Sn_IR)) & W5500::INT_SEND_OK) != W5500::INT_SEND_OK)
     {
         // @Jul.10, 2014 fix contant name, and udp sendto function.
         switch (net.sreg<uint8_t>(0, Sn_SR))
         {
         case W5500::SOCK_CLOSED:
-            close(0);
+            net.close(0);
             return 0;
             // break;
         case W5500::SOCK_UDP:
             // ARP timeout is possible.
-            if ((tmp_Sn_IR & net.INT_TIMEOUT) == net.INT_TIMEOUT)
+            if ((tmp_Sn_IR & W5500::INT_TIMEOUT) == W5500::INT_TIMEOUT)
             {
-                net.sreg<uint8_t>(0, Sn_IR, net.INT_TIMEOUT);
+                net.sreg<uint8_t>(0, Sn_IR, W5500::INT_TIMEOUT);
                 return 0;
             }
             break;
@@ -177,49 +176,127 @@ int serverExampleTCP() {
     // printf("Server done\n");
 }
 
-// int clientExampleTCP() {
-//     SPI spi(PB_15, PB_14, PB_13); // mosi, miso, sclk
-//     EthernetInterface net(&spi, PB_12, PA_10);
-//     spi.format(8,0); // 8bit, mode 0
-//     spi.frequency(1000000); // 1MHz
-//     wait_us(1000*1000); // 1 second for stable state
-//     printf("Starting client...\n");
-//     // Bring up the network interface
-//     int result = net.connect();
-//     if (result != 0) {
-//         printf("Error connecting: %d\n", result);
-//         return -1;
-//     }
-//     printf("Connected. IP address: %s\n", net.getIPAddress());
+int clientExampleTCP() 
+{
+    spi.format(8,0); // 8bit, mode 0
+    spi.frequency(1000000); // 1MHz
+    wait_us(1000*1000); // 1 second for stable state
+    printf("Starting client...\n");
+    // printf("Ptimer is %d\n",ptimer);
+    // uint8_t buffer=0x1388;
+    net.reset();
+    // wait_us(1000*1000);
+    // ptimer=net.getPTIMER();
+    // printf("Ptimer is %d\n",ptimer);
+    uint16_t buf=0;
+    // buf=net.getSn_DPORT(0);
+    // printf("port is %d\n",buf);
+    // net.spiWrite(MR,0x04,&buffer,1);
+    uint32_t ip=strToIP_(IP_Addr);
+    uint32_t subn=strToIP_(IP_Subnet);
+    uint32_t gateway=strToIP_(IP_Gateway);
+    net.reg_wr<uint32_t>(GAR, gateway);
+    net.reg_wr<uint32_t>(SUBR, subn);
+    for (int i = 0; i < 6; i++)
+        net.reg_wr<uint8_t>(SHAR + i, MAC_Addr[i]);
+    net.reg_wr<uint32_t>(SIPR, ip);
+    // buffer=0x01;
+    // net.spiWrite(IMR,0x04,&buffer,1);
+   
+    // net.sreg<uint8_t>(0, Sn_MR, Sn_MR_CLOSE);
+    net.sreg<uint8_t>(0, Sn_MR, Sn_MR_TCP);
+    uint8_t mrValue= net.getSn_MR(0);
+    printf("the value of MR is %d\n",mrValue);
+    net.sreg<uint16_t>(0, Sn_PORT, 80);
+    uint32_t dip=strToIP_(DIP_Addr);
+    net.sreg<uint16_t>(0,Sn_DPORT, 5000);
+    // uint8_t bufPort[2]={0x13,0x88};
+    // net.spiWrite(0x0010,0x0C,bufPort,2);
+    net.sreg<uint32_t>(0,Sn_DIPR, dip);
+    net.scmd(0, W5500::Command::OPEN);
+    while(net.getSn_SR(0)!= W5500::SOCK_INIT);
+    // buf=net.getSn_PORT(0);
+    buf=net.getSn_DPORT(0);
+    printf("port is %d\n",buf);
+    net.scmd(0, W5500::Command::CONNECT);
+    // printf("status register value= %d\n",net.getSn_SR(0));
+    while(!net.getSn_IR(0));
+    printf("Connection established\n");
+    // // Send data to the server
+    char *message = "Hello, Server!";
+    
+    int size,size2;
+     do
+        {
+            size = net.sreg<uint16_t>(0, Sn_TX_FSR);
+            size2 = net.sreg<uint16_t>(0, Sn_TX_FSR);
+        } while (size != size2);
+    uint16_t ptr = net.sreg<uint16_t>(0, Sn_TX_WR);
+    uint8_t cntl_byte = (0x14 + (0 << 5));
+    net.spiWrite(ptr, cntl_byte, (uint8_t *)message, strlen(message));
+    net.sreg<uint16_t>(0, Sn_TX_WR, ptr + (strlen(message)));
+    net.scmd(0, W5500::SEND);
+    uint8_t tmp_Sn_IR;
+    while (((tmp_Sn_IR = net.sreg<uint8_t>(0, Sn_IR)) & W5500::INT_SEND_OK) != W5500::INT_SEND_OK)
+    {
+        // @Jul.10, 2014 fix contant name, and udp sendto function.
+        switch (net.sreg<uint8_t>(0, Sn_SR))
+        {
+        case W5500::SOCK_CLOSED:
+            net.close(0);
+            return 0;
+            // break;
+        case W5500::SOCK_UDP:
+            // ARP timeout is possible.
+            if ((tmp_Sn_IR & W5500::INT_TIMEOUT) == W5500::INT_TIMEOUT)
+            {
+                net.sreg<uint8_t>(0, Sn_IR, W5500::INT_TIMEOUT);
+                return 0;
+            }
+            break;
+        default:
+            break;
+        }
+    }
+    net.sreg<uint8_t>(0, Sn_IR, W5500::INT_SEND_OK);
+    uint8_t buffer=0;
+    do{
+        buffer=net.sreg<uint8_t>(0, Sn_IR);
+    }while(buffer!=5);
+    size=0;
+    size2=0;
+     do
+        {
+            size = net.sreg<uint16_t>(0, Sn_RX_RSR);
+            size2 = net.sreg<uint16_t>(0, Sn_RX_RSR);
+        } while (size != size2);
+            // return size;
 
-//     // Create a TCP socket
-//     TCPSocketConnection socket;
-//     // socket.open(&net);
+        // if (wait_time_ms != (-1) && t.read_ms() > wait_time_ms)
+        // {
+        //     break;
+        // }
+    ptr = net.sreg<uint16_t>(0, Sn_RX_RD);
+    cntl_byte = (0x18 + (0 << 5));
+    char buffer2[256];
+    net.spiRead(ptr, cntl_byte, (uint8_t*)buffer2, size);
+    net.sreg<uint16_t>(0, Sn_RX_RD, ptr + size);
+    net.scmd(0, W5500::RECV);
+    int len=size;
+    if (len > 0) {
+        buffer2[len] = '\0'; // Null-terminate the received string
+        printf("Received: %s\n", buffer2);
+    }
+    
+    // socket.send(message, strlen(message));
 
-//     // Connect to the server
-//     result = socket.connect("192.168.11.194",5000);
-//     if (result != 0) {
-//         printf("Error connecting to server: %d\n", result);
-//         return -1;
-//     }
+    return 0;
 
-//     // Send data to the server
-//     char *message = "Hello, Server!";
-//     socket.send(message, strlen(message));
+    // // Close the socket
+    // socket.close();
 
-//     // Receive data from the server
-//     char buffer[256];
-//     int len = socket.receive(buffer, sizeof(buffer));
-//     if (len > 0) {
-//         buffer[len] = '\0'; // Null-terminate the received string
-//         printf("Received from server: %s\n", buffer);
-//     }
+    // // Bring down the network interface
+    // net.disconnect();
 
-//     // Close the socket
-//     socket.close();
-
-//     // Bring down the network interface
-//     net.disconnect();
-
-//     printf("Client done\n");
-// }
+    // printf("Client done\n");
+}
--- example/w5500example.hpp
+++ example/w5500example.hpp
@@ -2,7 +2,7 @@
 #ifndef DATA_BUF_SIZE
 	#define DATA_BUF_SIZE			2048
 #endif
-void clientExampleTCP();
+int clientExampleTCP();
 int serverExampleTCP();
 int32_t loopback_tcps(uint8_t sn, uint8_t* buf, uint16_t port);
 uint32_t strToIP_(const char *str);
